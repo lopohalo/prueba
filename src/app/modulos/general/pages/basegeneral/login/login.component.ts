659a47ed3855c5b77d985a3579645d6d08d0b31f
Juan Sebastian Rueda Tarazona <jsebg.ruedat@gmail.com>
2024-06-11 21:13:42 -0500

    se crea un login para demostrar la proteccion de rutas

--- src/app/modulos/general/pages/basegeneral/login/login.component.ts
+++ src/app/modulos/general/pages/basegeneral/login/login.component.ts
@@ -1,12 +1,16 @@
-import { OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
 
-export class LoginComponent implements OnInit {
-  loginForm: FormGroup | any;
-
-  constructor(private fb: FormBuilder) { }
-
-  ngOnInit() {
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) { 
     this.loginForm = this.fb.group({
       username: ['', Validators.required],
       password: ['', Validators.required]
@@ -15,15 +19,15 @@ export class LoginComponent implements OnInit {
 
   login() {
     if (this.loginForm.valid) {
-      const username = this.loginForm.get('username').value;
-      const password = this.loginForm.get('password').value;
-      
-      // Add your authentication logic here
-      console.log('Username:', username);
-      console.log('Password:', password);
-
-      // Reset the form after submission
-      this.loginForm.reset();
+      this.authService.login(this.loginForm.value.username, this.loginForm.value.password)
+        .subscribe(
+          (res:any) => {
+            // Redirect to 'tareas' route
+            this.router.navigate(['/tareas']);
+          },
+          (err:any) => {
+          }
+        );
     }
   }
 }
